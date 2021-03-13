52cebac718779f092413676c06baea04a244e290
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-13 19:43:38 +0900

    save方法

--- Assets/Presenter.cs
+++ Assets/Presenter.cs
@@ -8,11 +8,11 @@ public class Presenter : MonoBehaviour
 {
     //[SerializeField] GameObject image;
     //[SerializeField] TestScript testscript;
-    [SerializeField] Text textscropt;
+    //[SerializeField] Text textscropt;
     public void OnClockButton()
     {
 
-        Debug.Log("ボタンが押された");
+        //Debug.Log("ボタンが押された");
         //SceneManager.LoadScene("Main");
 
         //objectの非表示
@@ -21,12 +21,27 @@ public class Presenter : MonoBehaviour
         //obkectの破壊
         //Destroy(image);
         //testscript.TestScriptText();
-        textscropt.text = "haruyuki";
+        //textscropt.text = "haruyuki";
     }
-
+    //[SerializeField] GameObject imagePrefab;
+    //[SerializeField] Transform parent;
     void Start()
     {
-        
+        //オブジェクトの生成
+        //GameObject image = Instantiate(imagePrefab);
+
+        //親要素の変更
+        //image.transform.SetParent(parent);
+        /*
+        string xx = "saveデータ";
+        PlayerPrefs.SetString("DATA", xx);
+        PlayerPrefs.Save();
+        */
+
+        string saveText = PlayerPrefs.GetString("DATA");
+        Debug.Log(saveText);
+
+
     }
 
     
