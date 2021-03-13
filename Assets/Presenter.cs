8f0c8db61cc5e4387091b8ad7206ac7f7c24951d
haru0418 <77143892+haru0418@users.noreply.github.com>
2021-03-13 19:00:15 +0900

    Presenterファイル作成

---
+++ Assets/Presenter.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Presenter : MonoBehaviour
+{
+    [SerializeField] GameObject image;
+    public void OnClockButton()
+    {
+
+        Debug.Log("ボタンが押された");
+        //SceneManager.LoadScene("Main");
+
+        //objectの非表示
+        //image.SetActive(false);
+
+        //obkectの破壊
+        Destroy(image);
+    }
+
+    void Start()
+    {
+        
+    }
+
+    
+}
